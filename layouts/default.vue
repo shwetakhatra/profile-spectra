b83634a4ef6e7931fdf7945de8c2a2fb77828a83
Shweta Khatra <khatrashweta10314@gmail.com>
2024-01-18 09:17:47 +0100

    default view change

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,26 +1,33 @@
 <template>
     <div class="DefaultLayout">
-        <Loader :isLoading="loading" />
-        <Header v-if="!loading"/>
-        <slot />
+      <Loader :isLoading="loading" />
+      <Header v-if="!loading" />
+      <slot />
     </div>
-</template>
-
-<script>
-import Header from '@/components/AppHeader';
-import Loader from '~/components/Loader.vue';
-import { ref } from 'vue';
-
-const loading = ref(true);
-
-setTimeout(() => {
-  loading.value = false;
-}, 600);
-
-export default{
-    components:{
-        Header,
-        Loader
+  </template>
+  
+  <script>
+  import Header from '@/components/AppHeader';
+  import Loader from '~/components/Loader.vue';
+  import { ref, onMounted } from 'vue';
+  
+  export default {
+    components: {
+      Header,
+      Loader
+    },
+    setup() {
+      const loading = ref(true);
+  
+      onMounted(() => {
+        setTimeout(() => {
+          loading.value = false;
+        }, 600);
+      });
+      return {
+        loading
+      };
     }
-}
-</script>
+  }
+  </script>
+  
