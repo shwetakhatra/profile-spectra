31466289b0d36dace014d64f8b261f4c0d23b057
Shweta Khatra <khatrashweta10314@gmail.com>
2024-01-18 10:08:57 +0100

    default view change

--- components/ProfileCard.vue
+++ components/ProfileCard.vue
@@ -1,16 +1,17 @@
 <template>
-    <div class="card w-96 bg-base-100 shadow-xl m-5 relative min-h-80"  v-if="profile" @mouseleave="isHovered = false">
-      <figure class="px-10 pt-10" @mouseover="isHovered = true">
+  <div class="card-container">
+    <div class="card w-96 bg-base-100 shadow-xl m-5 relative min-h-80" v-if="profile" :class="{ 'flipped': isFlipped }" @mouseover="isFlipped = true" @mouseleave="isFlipped = false">
+      <figure class="px-10 pt-10" v-if="!isFlipped">
         <img v-if="profile.picture && profile.picture.large" :src="profile.picture.large" :alt="`Photo of ${profile.name.first}`" class="rounded-xl" />
       </figure>
       <div class="card-body items-center text-center mt-6">
-        <h2 v-if="profile" class="card-title">{{ profile.name.title }}. {{ profile.name.first }} {{ profile.name.last }}</h2>
+        <h2 v-if="profile && !isFlipped" class="card-title">{{ profile.name.title }}. {{ profile.name.first }} {{ profile.name.last }}</h2>
         <transition name="fade-slide">
-          <div v-if="isHovered" class="card-details">
-            <p class="font-semibold line-clamp-3"><font-awesome-icon icon="fa-solid fa-envelope" />  {{ profile.email }}</p>
-            <p class="font-semibold line-clamp-3"><font-awesome-icon icon="fa-solid fa-phone" />  {{ profile.phone }}</p>
-            <p class="font-semibold line-clamp-3"><font-awesome-icon icon="fa-solid fa-location-dot" />  {{ profile.location.street.name }}, {{ profile.location.city }}, {{profile.location.state }}</p>
-            <p class="font-semibold line-clamp-3"><font-awesome-icon icon="fa-solid fa-user" />  {{ profile.login.username }}</p>
+          <div v-if="isFlipped" class="card-details mt-3">
+            <p class="font-semibold mb-2"><font-awesome-icon icon="fa-solid fa-envelope" />  {{ profile.email }}</p>
+            <p class="font-semibold mb-2"><font-awesome-icon icon="fa-solid fa-phone" />  {{ profile.phone }}</p>
+            <p class="font-semibold mb-2"><font-awesome-icon icon="fa-solid fa-location-dot" />  {{ profile.location.street.name }}, {{ profile.location.city }}, {{profile.location.state }}</p>
+            <p class="font-semibold mb-5"><font-awesome-icon icon="fa-solid fa-user" />  {{ profile.login.username }}</p>
             <div class="card-actions justify-end">
               <button type="button" class="rounded bg-primary px-6 pb-2 pt-2.5 font-medium uppercase leading-normal text-white" @click="deleteProfile">Delete</button>
             </div>
@@ -18,6 +19,7 @@
         </transition>
       </div>
     </div>
+  </div>
   </template>
   
 <script setup lang="ts">
@@ -30,7 +32,7 @@
     },
   });
   const { profile } = props;
-  const isHovered = ref(false);
+  const isFlipped = ref(false);
   const emit = defineEmits();
 
   const deleteProfile = () => {
@@ -39,33 +41,18 @@
 </script>
   
 <style scoped>
-  .fade-slide-enter, .fade-slide-leave-to {
-    opacity: 0;
-    transform: translateY(20px);
+  .card-container {
+    perspective: 1000px;
   }
-
-  .fade-slide-enter-active, .fade-slide-leave-active {
-    transition: opacity 0.5s, transform 0.8s;
+  .card {
+    transform-style: preserve-3d;
+    transition: transform 1s;
   }
-  .card-body {
-    overflow: hidden;
+  .card:hover {
+    transform: rotateY(180deg);
   }
-  .card-details {
-    position: absolute;
-    left: 0;
-    right: 0;
-    padding: 1rem;
-    background-color: rgba(255, 255, 255, 0.9);
-    transform: translateY(100%);
-    transition: transform 0.8s ease-in-out;
-    z-index: 999;
-    border-bottom-left-radius: 18px;
-    border-bottom-right-radius: 18px;
-  }
-
-  .card:hover .card-details {
-    transform: translateY(0);
-    bottom: 0;
+  .card:hover .card-details{
+    transform: rotateY(180deg);
   }
 </style>
   
